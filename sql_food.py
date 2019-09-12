d9a1f95a8bdac4b02b4ba2a4ef512fc81d4f90bc
Zachary Cripps <zachary.cripps@griffithuni.edu.au>
2019-09-12 20:31:40 +1000

    created previolation table, inserted data

---
+++ sql_food.py
@@ -0,0 +1,33 @@
+import sqlite3
+connection=sqlite3.connect('database.db')
+cursor =connection.cursor()
+sql_command = "drop table if exists previousViolations ;"
+cursor.execute(sql_command)
+sql_command = """
+create table previousViolations(
+facility_name text ,
+facility_address text ,
+facility_zip text ,
+facility_city text 
+);
+"""
+cursor.execute(sql_command)
+bus_query ="""
+insert into previousViolations (facility_name,facility_address, facility_zip, facility_city)
+select I.facility_name,I.facility_address,I.facility_zip,I.facility_city
+from violations V, inspections I
+where  V.serial_number = I.serial_number
+group by facility_name
+"""
+cursor.execute(bus_query)
+cursor.execute("""
+select facility_name,count(violation_code)
+from violations V, inspections I
+where  V.serial_number = I.serial_number
+group by facility_name
+""")
+result = cursor.fetchall()
+for r in result:
+    print(r)
+connection.commit()
+connection.close()
