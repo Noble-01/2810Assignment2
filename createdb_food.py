3cd4d9cde6ed81ceaa3bce94d0665980ae1842cb
Zachary Cripps <zachary.cripps@griffithuni.edu.au>
2019-09-12 20:30:20 +1000

    changed data types

--- createdb_food.py
+++ createdb_food.py
@@ -19,25 +19,26 @@ violation_status text
 cursor.execute(sql_command)
 sql_command = """
 create table inspections(
-activity_date date not null,
-employee_id varchar(12) not null,
-facility_address varchar(120) not null,
-facility_city varchar(60) not null,
-facility_id varchar(12) not null,
-facility_name varchar(100) not null,
-facility_state varchar(2) not null,
-facility_zip varchar(10) not null,
-grade text(1) not null,
-owner_id varchar(12) not null,
-owner_name varchar(100) not null,
-pe_description text(200) not null,
-program_element_pe integer(4) not null,
-program_name varchar(100) not null,
-program_status text not null,
-record_id varchar(12) not null,
-score integer(3) not null,
-serial_number varchar(15) not null,
-service_description text(100) not null
+activity_date date ,
+employee_id text ,
+facility_address text ,
+facility_city text ,
+facility_id text ,
+facility_name text ,
+facility_state text ,
+facility_zip text ,
+grade text,
+owner_id text,
+owner_name text,
+pe_description text,
+program_element_pe int,
+program_name text,
+program_status text,
+record_id text ,
+score int,
+serial_number text ,
+service_code int,
+service_description text 
 );
 """
 cursor.execute(sql_command)
@@ -54,14 +55,11 @@ sheet_inspec = wb_inspections['inspections']
 
 
 viol_query = """ insert into violations (point, serial_number, violation_code, violation_description, violation_status) values (?,?,?,?,?)"""
-inspec_query=""" insert into violation values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) """
-for row in sheet_viol.iter_rows(min_row=2, max_row=20):
+inspec_query=""" insert into inspections values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) """
+for row in sheet_viol.iter_rows(min_row=2):
     cursor.execute(viol_query,[row[i].value for i in range(5)])
-for row in sheet_inspec.iter_rows(min_row=2, max_row=20):
+for row in sheet_inspec.iter_rows(min_row=2):
     cursor.execute(inspec_query,[row[i].value for i in range(20)])
-    #values = (row[0].value,row[1].value, row[2].value, row[3].value, row[4].value)
-    #print(point)  
-    #cursor.execute(query,values)
     
 connection.commit()
 connection.close()
